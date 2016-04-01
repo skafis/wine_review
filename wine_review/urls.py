49f76a559b1b17eca7d828f2d5f109416e3b1caf
FrankGwada <franciskipchumba5@gmail.com>
2016-04-01 12:06:42 +0000

    map urls to respective views

--- wine_review/urls.py
+++ wine_review/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^reviews/', include('reviews.urls' namespace="reviews"
+    url(r'^reviews/', include('reviews.urls', namespace="reviews")),
 ]
