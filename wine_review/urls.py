7fdea80d1b3b95ac1dfbaf1075aea88bd25ae22c
FrankGwada <franciskipchumba5@gmail.com>
2016-04-03 00:17:32 +0000

    add the login users link

--- wine_review/urls.py
+++ wine_review/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^reviews/', include('reviews.urls', namespace="reviews")),
+    url(r'^admin/', include(admin.site.urls)),
+    url('^accounts/', include('django.contrib.auth.urls'))
 ]
