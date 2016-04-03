ab3d7e32bed1212b57a90d6be5dc89395f5f9e50
FrankGwada <franciskipchumba5@gmail.com>
2016-04-03 01:02:45 +0000

    repaired the mainurls

--- wine_review/urls.py
+++ wine_review/urls.py
@@ -20,5 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^reviews/', include('reviews.urls', namespace="reviews")),
     url(r'^admin/', include(admin.site.urls)),
-    url('^accounts/', include('django.contrib.auth.urls'))
+    url('^accounts/', include('django.contrib.auth.urls', namespace="auth")),
 ]
