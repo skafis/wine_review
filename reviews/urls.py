2f74b7bdd4aced5fff4a8b377c9b515b96468761
FrankGwada <franciskipchumba5@gmail.com>
2016-04-12 06:00:26 +0000

    re mapped the urls properly

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,14 +1,13 @@
-from django.conf.urls import url
-from .import views
+from django.conf.urls import include, url
+from . import views
 
 urlpatterns = [
-    #ex: /
+    # ex: /
     url(r'^$', views.review_list, name='review_list'),
-    #ex: /review/5/
+    # ex: /review/5/
     url(r'^review/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
-    #ex : /wine/
+    # ex: /wine/
     url(r'^wine$', views.wine_list, name='wine_list'),
-    #ex: /wine/5/
-    url(r'^wine/(?P<wine_id>[0-9]+)/$',views.wine_detail, name='wine_detail'),
-    url(r'^wine/(?P<wine_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
-    ]
+    # ex: /wine/5/
+    url(r'^wine/(?P<wine_id>[0-9]+)/$', views.wine_detail, name='wine_detail'),
+]
