5fa93f0224d4db1f7b9a45e3703950cbd9dec379
FrankGwada <franciskipchumba5@gmail.com>
2016-04-12 06:08:24 +0000

    all templates working

--- wine_review/urls.py
+++ wine_review/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^reviews/', include('reviews.urls', namespace="reviews")),
     url(r'^admin/', include(admin.site.urls)),
-    url('^accounts/', include('django.contrib.auth.urls', namespace="auth")),
+    url(r'^', include('reviews.urls', namespace="reviews")),
+    # url('^accounts/', include('django.contrib.auth.urls', namespace="auth")),
 ]
