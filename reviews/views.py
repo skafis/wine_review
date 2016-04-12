4aaf4cac6c6384bac594aeb00cf87d84bbfbaffd
FrankGwada <franciskipchumba5@gmail.com>
2016-04-12 06:04:24 +0000

    reset the views to default

--- reviews/views.py
+++ reviews/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+# from django.http import HttpResponseRedirect
+# from django.core.urlresolvers import reverse
 from .models import Review, Wine
-from .forms import ReviewForm
-import datetime
+# from .forms import ReviewForm
+# import datetime
 # Create your views here.
 
-def user_review_list(request, username=None):
-    if not username:
-        username = request.user.username
+def review_list(request, username=None):
+    # if not username:
+        # username = request.user.username
     latest_review_list = Review.objects.order_by('-pub_date')[:9]
     context = {'latest_review_list' :latest_review_list}
     return render(request, 'reviews/review_list.html', context)
@@ -24,28 +24,28 @@ def wine_list(request):
     
 def wine_detail(request, wine_id):
     wine = get_object_or_404(Wine, pk=wine_id)
-    form = ReviewForm()
-    return render(request, 'reviews/wine_detail.html', {'wine' : wine, 'form': form})
+    # form = ReviewForm()
+    return render(request, 'reviews/wine_detail.html', {'wine' : wine, })
 
-@login_required
-def add_review(request, wine_id):
-    wine = get_object_or_404(Wine, pk=wine_id)
-    form = ReviewForm(request.POST)
-    if form.is_valid():
-        rating = form.cleaned_data['rating']
-        comment = form.cleaned_data['comment']
-        user_name = form.cleaned_data['user_name']
-        user_name = request.user.username
-        review = Review()
-        review.wine = wine
-        review.user_name = user_name
-        review.rating = rating
-        review.comment = comment
-        review.pub_date = datetime.datetime.now()
-        review.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('reviews:wine_detail', args=(wine.id,)))
+# # @login_required
+# def add_review(request, wine_id):
+#     wine = get_object_or_404(Wine, pk=wine_id)
+#     form = ReviewForm(request.POST)
+#     if form.is_valid():
+#         rating = form.cleaned_data['rating']
+#         comment = form.cleaned_data['comment']
+#         user_name = form.cleaned_data['user_name']
+#         user_name = request.user.username
+#         review = Review()
+#         review.wine = wine
+#         review.user_name = user_name
+#         review.rating = rating
+#         review.comment = comment
+#         review.pub_date = datetime.datetime.now()
+#         review.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('reviews:wine_detail', args=(wine.id,)))
 
-    return render(request, 'reviews/wine_detail.html', {'wine': wine, 'form': form})
+    # return render(request, 'reviews/wine_detail.html', {'wine': wine, 'form': form})
