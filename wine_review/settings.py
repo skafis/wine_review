fd425479d3302e25551d2e44b52f16468a1bce18
FrankGwada <franciskipchumba5@gmail.com>
2016-04-19 16:19:47 +0000

    include the root static files

--- wine_review/settings.py
+++ wine_review/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'wine_review.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'reviews/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,5 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'/static/')
 LOGIN_REDIRECT_URL = '/reviews/review/user'
 
