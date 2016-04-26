b15ee6ed5cfbe4fde047b5e4108666e2b546dd4f
skafis <franciskipchumba5@gmail.com>
2016-04-26 23:28:02 +0300

    fixed bugs

--- wine_review/urls.py
+++ wine_review/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf import settings
-from  django.conf.urls.static import include, url
+from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -26,4 +26,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
