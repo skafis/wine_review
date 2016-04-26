310885d1c0fb3b49615359d94217014e2482ad35
FrankGwada <franciskipchumba5@gmail.com>
2016-04-26 10:18:10 +0000

    styled using foundation and css

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     # ex: /
-    url(r'^$', views.review_list, name='review_list'),
+    url(r'^$', views.wine_list, name='wine_list'),
     # ex: /review/5/
     url(r'^review/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
     # ex: /wine/
-    url(r'^wine$', views.wine_list, name='wine_list'),
+    url(r'^wine$', views.review_list, name='review_list'),
     # ex: /wine/5/
     url(r'^wine/(?P<wine_id>[0-9]+)/$', views.wine_detail, name='wine_detail'),
     url(r'^wine/(?P<wine_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
-    url(r'^user/$', views.user_review_list, name='user'), 
+    url(r'^user/$', views.user_review_list, name='user') 
 ]
